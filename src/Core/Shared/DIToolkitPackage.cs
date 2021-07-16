e49da5fe7114174304b944809200f8f65e89545d
Steven Rasmussen <steven.rasmussen@gmail.com>
2021-07-16 13:08:28 -0600

    Initial check-in

---
+++ src/Core/Shared/DIToolkitPackage.cs
@@ -0,0 +1,70 @@
+﻿using System.Threading;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+using Community.VisualStudio.Toolkit.DependencyInjection.Core;
+
+namespace Community.VisualStudio.Toolkit.DependencyInjection
+{
+    /// <summary>
+    /// Package that contains a DI service container.
+    /// </summary>
+    /// <typeparam name="TPackage"></typeparam>
+    [ProvideService(typeof(SToolkitServiceProvider<>), IsAsyncQueryable = true)]
+    public abstract class DIToolkitPackage<TPackage> : ToolkitPackage
+        where TPackage : AsyncPackage
+    {
+        /// <summary>
+        /// Custom ServiceProvider for the package.
+        /// </summary>
+        public IToolkitServiceProvider<TPackage> ServiceProvider { get; private set; } = null!; // This property is initialized in `InitializeAsync`, so it's never actually null.
+
+        /// <summary>
+        /// Initializes the <see cref="AsyncPackage"/>
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <param name="progress"></param>
+        /// <returns></returns>
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await base.InitializeAsync(cancellationToken, progress);
+
+            IServiceCollection services = this.CreateServiceCollection();
+            InitializeServices(services);
+
+            IServiceProvider serviceProvider = BuildServiceProvider(services);
+            ServiceProvider = new ToolkitServiceProvider<TPackage>(serviceProvider);
+
+            // Add the IToolkitServiceProvider to the VS IServiceProvider
+            AsyncServiceCreatorCallback serviceCreatorCallback = (sc, ct, t) =>
+            {
+                return Task.FromResult((object)this.ServiceProvider);
+            };
+
+            AddService(typeof(SToolkitServiceProvider<TPackage>), serviceCreatorCallback, true);
+        }
+
+        /// <summary>
+        /// Create the service collection.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract IServiceCollection CreateServiceCollection();
+
+        /// <summary>
+        /// Builds the service collection
+        /// </summary>
+        /// <param name="serviceCollection"></param>
+        /// <returns></returns>
+        protected abstract IServiceProvider BuildServiceProvider(IServiceCollection serviceCollection);
+
+        /// <summary>
+        /// Initialize the services in the DI container.
+        /// </summary>
+        /// <param name="services"></param>
+        protected virtual void InitializeServices(IServiceCollection services)
+        {
+            // Nothing
+        }
+    }
+}
---
+++ test/VSSDK.TestExtension/TestExtensionPackage.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading;
+using Community.VisualStudio.Toolkit.DependencyInjection.Microsoft;
+using Microsoft.VisualStudio.Shell;
+using TestExtension;
+using TestExtension.Commands;
+using Task = System.Threading.Tasks.Task;
+
+namespace VSSDK.TestExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuids.DITestExtensionString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class TestExtensionPackage : MicrosoftDIToolkitPackage<TestExtensionPackage>
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await base.InitializeAsync(cancellationToken, progress);
+
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            // Commands
+            await DependencyInjectionCommand.InitializeAsync(this);
+        }
+    }
+}
