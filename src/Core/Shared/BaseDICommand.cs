e10cf983550262aaf38fece5757b9d26fe80a4c1
Steven Rasmussen <steven.rasmussen@gmail.com>
2022-01-22 13:36:30 -0700

    Added the ability to register a command in the DI container. This also takes care of registering the command with Visual Studio.

---
+++ src/Core/Shared/BaseDICommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Community.VisualStudio.Toolkit.DependencyInjection.Core
+{
+    /// <summary>
+    /// Base class used for commands instantiated by the DI container.
+    /// </summary>
+    public abstract class BaseDICommand
+    {
+        private static readonly PropertyInfo _commandPropertyInfo = typeof(CommandWrapper<>).GetProperty(nameof(Command));
+
+        /// <summary>
+        /// The package.
+        /// </summary>
+        public DIToolkitPackage Package { get; }
+
+        /// <summary>
+        /// The command object associated with the command ID (GUID/ID).
+        /// </summary>
+        public OleMenuCommand Command { get; }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="package"></param>
+        public BaseDICommand(DIToolkitPackage package)
+        {
+            this.Package = package;
+            var commandWrapperType = typeof(CommandWrapper<>).MakeGenericType(this.GetType());
+            var commandWrapper = package.ServiceProvider.GetRequiredService(commandWrapperType);
+            PropertyInfo commandPropertyInfo = commandWrapperType.GetProperty(nameof(Command));
+            this.Command = (OleMenuCommand)commandPropertyInfo.GetValue(commandWrapper, null);
+        }
+
+        /// <summary>
+        /// Execute the command logica
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected internal virtual void Execute(object sender, EventArgs e)
+        {
+            Package.JoinableTaskFactory.RunAsync(async delegate
+            {
+                try
+                {
+                    await ExecuteAsync((OleMenuCmdEventArgs)e);
+                }
+                catch (Exception ex)
+                {
+                    await ex.LogAsync();
+                }
+            }).FireAndForget();
+        }
+
+        /// <summary>Executes asynchronously when the command is invoked and <see cref="Execute(object, EventArgs)"/> isn't overridden.</summary>
+        /// <remarks>Use this method instead of <see cref="Execute"/> if you're invoking any async tasks by using async/await patterns.</remarks>
+        protected internal virtual Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// BeforeQueryStatus
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        protected internal virtual void BeforeQueryStatus(object sender, EventArgs e)
+        {
+            BeforeQueryStatus(e);
+        }
+
+        /// <summary>Override this method to control the commands visibility and other properties.</summary>
+        protected virtual void BeforeQueryStatus(EventArgs e)
+        {
+            // Leave empty
+        }
+    }
+}
---
+++ src/Core/Shared/CommandWrapper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Shell;
+using Task = System.Threading.Tasks.Task;
+
+namespace Community.VisualStudio.Toolkit.DependencyInjection.Core
+{
+    internal class CommandWrapper<T>
+        where T : BaseDICommand
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public CommandWrapper(IServiceProvider serviceProvider, AsyncPackage package)
+        {
+            this._serviceProvider = serviceProvider;
+
+            CommandAttribute? attr = (CommandAttribute)typeof(T).GetCustomAttributes(typeof(CommandAttribute), true).FirstOrDefault();
+
+            if (attr is null)
+            {
+                throw new InvalidOperationException($"No [Command(GUID, ID)] attribute was added to {typeof(T).Name}");
+            }
+
+            // Use package GUID if no command set GUID has been specified
+            Guid cmdGuid = attr.Guid == Guid.Empty ? package.GetType().GUID : attr.Guid;
+            CommandID cmd = new(cmdGuid, attr.Id);
+
+            this.Command = new OleMenuCommand(this.Execute, changeHandler: null, this.BeforeQueryStatus, cmd);
+
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            IMenuCommandService commandService = serviceProvider.GetRequiredService<IMenuCommandService>();
+            commandService.AddCommand(this.Command);  // Requires main/UI thread
+        }
+
+        /// <summary>
+        /// The command object associated with the command ID (GUID/ID).
+        /// </summary>
+        public OleMenuCommand Command { get; }
+
+        protected void BeforeQueryStatus(object sender, EventArgs e)
+        {
+            using var scope = this._serviceProvider.CreateScope();
+            var instance = (BaseDICommand)scope.ServiceProvider.GetRequiredService(typeof(T));
+            instance.BeforeQueryStatus(sender, e);
+        }
+
+        protected void Execute(object sender, EventArgs e)
+        {
+            using var scope = this._serviceProvider.CreateScope();
+            var instance = (BaseDICommand)scope.ServiceProvider.GetRequiredService(typeof(T));
+            instance.Execute(sender, e);
+        }
+
+        protected async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            using var scope = this._serviceProvider.CreateScope();
+            var instance = (BaseDICommand)scope.ServiceProvider.GetRequiredService(typeof(T));
+            await instance.ExecuteAsync(e);
+        }
+    }
+}
