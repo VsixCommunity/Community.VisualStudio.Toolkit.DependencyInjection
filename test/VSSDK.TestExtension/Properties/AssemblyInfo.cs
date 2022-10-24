4758e036752cae4eba96a9c54b75021fc6c5fb07
Steven Rasmussen <steven.rasmussen@gmail.com>
2022-10-23 22:54:33 -0600

    Updated to use the latest package of the community toolkit.

--- test/VSSDK.TestExtension/Properties/AssemblyInfo.cs
+++ test/VSSDK.TestExtension/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
 ﻿using Microsoft.VisualStudio.Shell;
 
 [assembly: ProvideCodeBase(AssemblyName = "Community.VisualStudio.Toolkit")]
+
+[assembly: ProvideBindingRedirection(AssemblyName = "Community.VisualStudio.Toolkit",
+    NewVersion = "17.0.475.0", OldVersionLowerBound = "17.0.430.0", OldVersionUpperBound = "17.0.451.0")]
